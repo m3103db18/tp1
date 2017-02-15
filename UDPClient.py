c0242e75dffd4344f82ddd953fc406ee12d992c2
m3103db18 <mahmoudhanfaoui@hotmail.fr>
2017-02-15 14:55:29 +0100

    Add files via upload

---
+++ UDPClient.py
@@ -0,0 +1,17 @@
+
+from socket import *
+
+serverName = "localhost"
+serverPort = 12000
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+message= raw_input('texte? ')
+
+clientSocket.sendto(message,(serverName, serverPort))
+
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048) 
+
+print modifiedMessage
+
+clientSocket.close()
